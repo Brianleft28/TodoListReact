7c75a216aebd2f59c1d469039a14cc349b421a87
BrianLEFT <contactobrianleft@gmail.com>
2024-05-13 16:32:06 -0300

    In Process: Modal TaskForm

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TaskForm = () => {
+const TaskForm = ({ onSubmit }) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Llama a la función onSubmit pasando los datos de la nueva tarea
+        onSubmit({ title, description });
+        setTitle('');
+        setDescription('');
+      };
+
   return (
-    <div className='form'>
-        <input type="text" name="taskForm" id="taskForm" />
-        <button>Agregar Tarea</button>
-    </div>    
+   <form onSubmit={handleSubmit}>
+        <input type="text" 
+        placeholder='Título'
+        value={title}
+        onChange={(e)=> setTitle(e.target.value)}
+        required
+        />
+        <textarea  
+        placeholder='Descripción'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit">Añadir Tarea</button>
+   </form>
   )
 }
 
