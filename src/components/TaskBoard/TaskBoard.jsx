32e0eb7c2b71e61117be96ec3aa3fb1883649bad
BrianLEFT <contactobrianleft@gmail.com>
2024-05-16 14:54:39 -0300

    table responsive

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -18,12 +18,12 @@ if (tasks.length === 0 || tasks === '') {
 
   return (
 <div className=' mt-3 flex justify-center'>
-  <div className='overflow-x-auto align-middle max-w-[985px] md:w-screen flex justify-center items-center'>
+  <div className=' align-middle max-w-[985px] md:w-screen flex justify-center items-center'>
     <table className="shadow-lg max-w-full w-full table-auto border-collapse border-y-2 border-x-2  border-green-500">
       <thead>
         <tr className="selection:bg-none hover:cursor-default bg-green-500 border-b-4 border-gray-500 text-white">
           <th className="border border-green-500  px-4 py-2">Título</th>
-          <th className="border border-gray-200 px-4 py-2">Descripción</th>
+          <th className="border border-green-500 px-4 py-2">Descripción</th>
         </tr>
       </thead>
       <tbody >
