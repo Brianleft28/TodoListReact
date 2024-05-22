0b53e5dc9c749e99a70b224bfe3b7e36f552d970
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-05-22 17:47:28 -0300

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
-const flowbite = require("flowbite-react/tailwind");
+const flowbite = require('flowbite-react/tailwind');
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
     flowbite.content(),
-  ],  
+  ],
   theme: {
     extend: {},
   },
   daisyui: {
-    darkTheme: "dim",
-    lightTheme: "lemonade",
-    themes: ["lemonade", "dim"],
+    darkTheme: 'sunset',
+    lightTheme: 'lemonade',
+    themes: ['lemonade', 'sunset'],
   },
-  plugins: [require('flowbite/plugin'),require('daisyui'),],
-}
-
+  plugins: [require('flowbite/plugin'), require('daisyui')],
+};
