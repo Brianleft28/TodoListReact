ebc070c8ea985e0ce4f76fe9b0df590bdf7b8a26
BrianLEFT <contactobrianleft@gmail.com>
2024-05-23 16:28:34 -0300

    construyendo logica de las estadisticas

--- src/pages/estadisticas.jsx
+++ src/pages/estadisticas.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Card, Divider } from 'react-daisyui';
+import TaskContext from '../context/TaskContext';
+
 
 const Estadisticas = () => {
-  return <div>Estadisticas</div>;
+const {tasks} = useContext(TaskContext);
+  return ( 
+  <>
+  <div className="p-4 flex flex-col w-full lg:flex-row">
+  <Card className="grid flex-grow h-32 bg-base-300 rounded-box place-items-center">
+  <span className='font-semibold'>Tareas Completas</span>
+  <span>{tasks.filter((task) => task.status === 'Completo').length}</span>
+
+  </Card>
+  <Divider> </Divider>
+  <Divider> </Divider>
+  <Divider> </Divider>
+  <Card className="grid flex-grow h-32 bg-base-300 rounded-box place-items-center">
+    Tareas Pendientes
+  </Card>
+</div>
+ <div className="p-4 flex flex-col w-full lg:flex-row">
+  {/*  */}
+  <Card className="grid flex-grow h-32 bg-base-300 rounded-box place-items-center">
+  Tareas en proceso
+  </Card>
+  <Divider> </Divider>
+  <Divider> </Divider>
+  <Divider> </Divider>
+  <Card className="grid flex-grow h-32 bg-base-300 rounded-box place-items-center">
+    Tareas paradas
+  </Card>
+ </div>
+  </>
+  )
 };
 
 export default Estadisticas;
